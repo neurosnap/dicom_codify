ff4c6b8407f41e1945cdac55a9661a5102076038
Eric Bower <neurosnap@gmail.com>
2014-10-21 00:12:39 -0400

    project rename complete

--- tasks.py
+++ tasks.py
@@ -1,14 +1,14 @@
-""" DICOM Codified Task Runner
+""" DICOM Codify Task Runner
 A convenient way to execute tasks for a python package
 """
 from invoke import run, task
 
-from dicom_deidentify import soup, create_json
+from dicom_codify import soup, create_json
 
 @task(help={"fname": "File name to save JSON output"})
 def ac(fname=None):
     """ Action Codes """
-    from dicom_deidentify import get_action_codes
+    from dicom_codify import get_action_codes
     output = get_action_codes(soup())
 
     if fname is not None:
@@ -21,7 +21,7 @@ def ac(fname=None):
 @task(help={"fname": "File name to save JSON output"})
 def di(fname=None):
     """ De-identifers """
-    from dicom_deidentify import get_deidentify
+    from dicom_codify import get_deidentify
     output = get_deidentify(soup())
 
     if fname is not None:
@@ -34,7 +34,7 @@ def di(fname=None):
 @task(help={"fname": "File name to save JSON output"})
 def ded(fname=None):
     """ Data Element Dictionary """
-    from dicom_deidentify import get_data_element_dictionary
+    from dicom_codify import get_data_element_dictionary
     output = get_data_element_dictionary(soup(part=6))
 
     if fname is not None:
